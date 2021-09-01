e0446ddfddcc7a27282e5c6990d4c73ca13f64af
Umreen <uidigitalcrafts@gmail.com>
2021-08-31 23:00:16 -0500

    Continuing to clean data and run analyses

--- gas_turbine.py
+++ gas_turbine.py
@@ -15,9 +15,11 @@ import numpy as np
 import pandas as pd 
 import seaborn as sns
 import matplotlib.pyplot as plt 
+import statsmodels.formula.api as smf
+import statsmodels.api as sm
+from statsmodels.stats.diagnostic import normal_ad
 
-from plotnine import ggplot, aes, geom_histogram, labs
-
+from plotnine import ggplot, aes, geom_histogram, labs, geom_boxplot
 
 """""""""""""""
 FUNCTIONS
@@ -40,7 +42,7 @@ NOX = gt_data.iloc[:, 10]
 # Carbon monoxide distribution
 (
      ggplot(gt_data, aes(x = CO)) + 
-     geom_histogram(binwidth = 2, 
+     geom_histogram(binwidth = 0.5, 
                     fill = 'pink', 
                     colour = 'red', 
                     size = 0.5, 
@@ -54,7 +56,7 @@ NOX = gt_data.iloc[:, 10]
 # Nitrogen oxide (NOX) distribution
 (
      ggplot(gt_data, aes(x = NOX)) + 
-     geom_histogram(binwidth = 0.5, 
+     geom_histogram(binwidth = 2, 
                     fill = 'pink', 
                     colour = 'red', 
                     size = 0.5, 
@@ -63,3 +65,81 @@ NOX = gt_data.iloc[:, 10]
           x = "NOX", 
           y = "Count")
 )
+
+# Boxplots for CO and NOX 
+plt.boxplot([CO, NOX])
+plt.xticks([1, 2], ["CO", "NOX"])
+plt.title("CO and NOX Emissions Boxplot")
+
+# Remove outliers    
+gt_filtered = gt_data[(gt_data.CO > 0.5) & (gt_data.CO < 2.5) 
+                      & (gt_data.NOX > 25) & (gt_data.NOX < 90)]
+
+# Re-plot CO & NOX data
+CO_new = gt_filtered.iloc[:, 9]
+NOX_new = gt_filtered.iloc[:, 10]
+
+(
+     ggplot(gt_filtered, aes(x = CO_new)) + 
+     geom_histogram(binwidth = 0.5,
+                    bins = 20,
+                    fill = 'pink', 
+                    colour = 'red', 
+                    size = 0.5, 
+                    alpha = 0.5) + 
+     labs(title = "Carbon Monoxide Distribution", 
+          x = "CO", 
+          y = "Count")
+)
+
+
+# Nitrogen oxide (NOX) distribution
+(
+     ggplot(gt_filtered, aes(x = NOX_new)) + 
+     geom_histogram(binwidth = 2, 
+                    fill = 'pink', 
+                    colour = 'red', 
+                    size = 0.5, 
+                    alpha = 0.5) + 
+     labs(title = "Nitrogen Oxide Distribution", 
+          x = "NOX", 
+          y = "Count")
+)
+
+# Create visual correlation between attributes
+corr_mat = gt_filtered.corr()
+plt.figure(figsize = (16, 10))
+sns.heatmap(corr_mat, annot = True)
+plt.show()
+
+# Create linear regression models
+for col in gt_filtered.columns:
+    print(col)
+
+# NOX model
+reg_NOX = smf.ols('NOX ~ AT + AP + AH + AFDP + GTEP + TIT + TAT + TEY + CDP + CO', 
+              data = gt_filtered)
+
+res_NOX = reg_NOX.fit()
+print(res_NOX.summary())
+
+# CO model
+reg_CO = smf.ols('CO ~ AT + AP + AH + AFDP + GTEP + TIT + TAT + TEY + CDP + NOX', 
+              data = gt_filtered)
+
+res_CO = reg_CO.fit()
+print(res_CO.summary())
+
+# Plotting residuals of both CO and NOX models to check normality
+# NOX model
+sm.qqplot(res_NOX.resid)
+
+# CO model
+sm.qqplot(res_CO.resid)
+
+# Perform Anderson-Darling normality test
+# NOX model
+normal_ad(res_NOX.resid)
+
+# CO model
+normal_ad(res_CO.resid)
